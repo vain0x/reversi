0188204de7299dcb0ec6a41fdbe024c5a0c2f801
vain0x <vainzerox@gmail.com>
2020-12-27 17:43:56 +0900

    feat: 盤面に石を配置する

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,26 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
+const ReversiBoard: React.FC = () => {
+  const cells: number[] = []
+  for (let i = 0; i < 8; i++) {
+    cells.push(i)
+  }
+
+  return <article className="g-reversi-board">
+    {cells.map(y => (<React.Fragment key={y}>
+      {cells.map(x => (<div key={x} className="cell">
+        <div className="stone" data-color={[null, "black", "white"][(y + x) % 3]} />
+      </div>))}
+    </React.Fragment>))}
+  </article>
+}
+
 const Main: React.FC = () => {
-  return <>
-    Hello, world!
-  </>
+  return <main id="main">
+    <h1>Reversi</h1>
+    <ReversiBoard />
+  </main>
 }
 
 export const main = () => {
