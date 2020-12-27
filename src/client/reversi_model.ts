ca5f7fe009a542909421a14887189d4da2a3f3b8
vain0x <vainzerox@gmail.com>
2020-12-27 22:07:01 +0900

    refactor: ファイルを分ける

---
+++ src/client/reversi_model.ts
@@ -0,0 +1,252 @@
+// リバーシモデル
+
+const EPS = 1e-6
+
+const exhaust = (never: never): never => never
+
+// -----------------------------------------------
+// Color
+// -----------------------------------------------
+
+export type Color = "WHITE" | "BLACK"
+
+export const flipColor = (color: Color): Color => {
+  switch (color) {
+    case "WHITE":
+      return "BLACK"
+
+    case "BLACK":
+      return "WHITE"
+
+    default:
+      throw exhaust(color)
+  }
+}
+
+// -----------------------------------------------
+// Pos
+// -----------------------------------------------
+
+/**
+ * セル番号 (左上から数える)
+ */
+export type CellId = number
+
+/**
+ * 位置 (行番号、列番号)
+ */
+export type Pos = [number, number]
+
+const idToPos = (id: CellId): Pos => [Math.floor(id / 8 + EPS), id % 8]
+
+const posToId = (pos: Pos): CellId => {
+  const [y, x] = pos
+  return y * 8 + x
+}
+
+const posIsValid = (pos: Pos): boolean => {
+  const [y, x] = pos
+  return 0 <= y && y < 8 && 0 <= x && x < 8
+}
+
+const posAdd = (l: Pos, r: Pos): Pos => {
+  const [l1, l2] = l
+  const [r1, r2] = r
+  return [l1 + r1, l2 + r2]
+}
+
+// -----------------------------------------------
+// newCells
+// -----------------------------------------------
+
+export const newCells = (): Cells => {
+  const cells: Cells = []
+  for (let y = 0; y < 8; y++) {
+    for (let x = 0; x < 8; x++) {
+      cells.push(null)
+    }
+  }
+
+  const initial: [CellId, Color][] = [
+    [27, "BLACK"],
+    [28, "WHITE"],
+    [35, "WHITE"],
+    [36, "BLACK"],
+  ]
+  for (const [i, color] of initial) {
+    cells[i] = color
+  }
+  return cells
+}
+
+// -----------------------------------------------
+// flip
+// -----------------------------------------------
+
+export type FlipResult = {
+  middles: CellId[]
+  ends: CellId[]
+}
+
+type Cells = Array<Color | null>
+
+const EMPTY_FLIP_RESULT: FlipResult = {
+  middles: [],
+  ends: [],
+}
+
+/**
+ * id に石を置いたときにひっくり返る石のリスト。
+ */
+const flippedCells = (cells: Cells, id: CellId, active: Color): FlipResult | null => {
+  const search = (d: Pos, middles: CellId[]): FlipResult | null => {
+    let p = idToPos(id)
+    while (true) {
+      p = posAdd(p, d)
+      if (!posIsValid(p)) {
+        return null
+      }
+
+      const i = posToId(p)
+      const c = cells[i]
+      if (c == null) {
+        return null
+      }
+
+      if (c === active) {
+        if (middles.length == 0) {
+          return null
+        }
+        return { middles, ends: [i] }
+      }
+
+      middles.push(posToId(p))
+    }
+  }
+
+  if (cells[id] != null) {
+    return null
+  }
+
+  const dx = [1, 1, 0, -1, -1, -1, 0, 1]
+  const dy = [0, 1, 1, 1, 0, -1, -1, -1]
+  const middles: CellId[] = []
+  const ends: CellId[] = []
+  for (let i = 0; i < 8; i++) {
+    const result = search([dy[i], dx[i]], [])
+    if (result != null) {
+      middles.push(...result.middles)
+      ends.push(...result.ends)
+    }
+  }
+
+  if (middles.length === 0) {
+    return null
+  }
+  return { middles, ends }
+}
+
+// -----------------------------------------------
+// put
+// -----------------------------------------------
+
+interface PutResult {
+  updateActive: (active: Color) => Color
+  updateCells: (cells: Cells) => Cells
+}
+
+export const put = (cells: Cells, id: number, active: Color): PutResult | null => {
+  // すでに石があったら置けない
+  if (cells[id] != null) {
+    return null
+  }
+
+  const result = flippedCells(cells, id, active)
+  if (result == null) {
+    return null
+  }
+
+  return {
+    updateActive: flipColor,
+    updateCells: cells => cells.map((color, i) => (
+      i === id || result.middles.includes(i)
+        ? active
+        : color
+    )),
+  }
+}
+
+interface PassResult {
+  updateActive: (active: Color) => Color
+}
+
+export const pass = (state: GameStateDerived): PassResult | null => {
+  if (!state.passOnly) {
+    return null
+  }
+
+  return {
+    updateActive: flipColor,
+  }
+}
+
+// -----------------------------------------------
+// GameState
+// -----------------------------------------------
+
+interface GameState {
+  active: Color
+  cells: Cells
+}
+
+export interface GameStateDerived extends GameState {
+  passOnly: boolean
+  blackCount: number
+  whiteCount: number
+  winner: Color | null
+  prediction: FlipResult[]
+}
+
+const getWinner = (blackCount: number, whiteCount: number): Color | null => {
+  if (blackCount == 0) {
+    return "WHITE"
+  }
+
+  if (whiteCount === 0) {
+    return "BLACK"
+  }
+
+  // 同数のときは後手の勝ち。
+  if (blackCount + whiteCount === 8 * 8) {
+    return blackCount > whiteCount ? "BLACK" : "WHITE"
+  }
+
+  return null
+}
+
+export const compute = (state: GameState): GameStateDerived => {
+  const { active, cells } = state
+
+  const blackCount = cells.filter(c => c === "BLACK").length
+  const whiteCount = cells.filter(c => c === "WHITE").length
+
+  const winner = getWinner(blackCount, whiteCount)
+
+  const prediction: GameStateDerived["prediction"] = winner == null
+    ? cells.map((_, i) => flippedCells(cells, i, active) ?? EMPTY_FLIP_RESULT)
+    : cells.map(() => EMPTY_FLIP_RESULT)
+
+  const passOnly =
+    winner == null
+    && prediction.every(result => result == null)
+
+  return {
+    active,
+    cells,
+    blackCount,
+    whiteCount,
+    prediction,
+    passOnly,
+    winner,
+  }
+}
---
+++ src/client/reversi_view.tsx
@@ -0,0 +1,159 @@
+import * as React from "react"
+import * as M from "./reversi_model"
+import {
+  CellId,
+  Color,
+  GameStateDerived,
+} from "./reversi_model"
+
+export const ReversiContainer: React.FC = () => {
+  const [active, setActive] = React.useState("BLACK" as Color)
+  const [cells, setCells] = React.useState(M.newCells())
+  const [hoveredCell, setHoveredCell] = React.useState<CellId | null>(null)
+
+  const state = React.useMemo(() =>
+    M.compute({ active, cells }),
+    [active, cells],
+  )
+
+  const hover = React.useCallback((id: CellId | null): void => {
+    setHoveredCell(id)
+  }, [])
+
+  const put = React.useCallback((id: CellId): void => {
+    const result = M.put(cells, id, active)
+    if (result != null) {
+      setActive(result.updateActive)
+      setCells(result.updateCells)
+    }
+  }, [active, cells])
+
+  const pass = React.useCallback(() => {
+    const result = M.pass(state)
+    if (result != null) {
+      setActive(result.updateActive)
+    }
+  }, [active, state])
+
+  const { blackCount, whiteCount, passOnly, winner } = state
+
+  return (
+    <article className="g-reversi g-reversi-container">
+      <Board
+        state={state}
+        hoveredCell={hoveredCell}
+        hover={hover}
+        put={put} />
+
+      <div>黒: {blackCount}</div>
+      <div>白: {whiteCount}</div>
+
+      {winner == null ? (
+        <div>
+          手番: {active === "BLACK" ? "黒" : "白"}
+        </div>
+      ) : (
+          <div className="winner">
+            勝者: {winner === "BLACK" ? "黒" : "白"}
+          </div>
+        )}
+
+      {passOnly ? (
+        <>
+          <div>
+            石を置く場所がありません。
+          </div>
+
+          <button type="button" onClick={pass}>
+            パス
+          </button>
+        </>
+      ) : null}
+    </article>
+  )
+}
+
+interface ReversiBoardProps {
+  state: GameStateDerived
+  hoveredCell: CellId | null
+  hover: (id: number | null) => void
+  put: (id: number) => void
+}
+
+const Board: React.FC<ReversiBoardProps> = props => {
+  const { state, hoveredCell, hover, put } = props
+  const { active, cells, prediction } = state
+
+  const onMouseLeave = React.useCallback(() => {
+    hover(null)
+  }, [hover])
+
+  const predictionTargets = React.useMemo(() =>
+    hoveredCell != null && prediction[hoveredCell] != null
+      ? [
+        ...prediction[hoveredCell]?.middles,
+        ...prediction[hoveredCell]?.ends,
+      ]
+      : [],
+    [hoveredCell, prediction],
+  )
+
+  return (
+    <article className="board" onMouseLeave={onMouseLeave}>
+      {cells.map((color, id) => {
+        const isCandidate = (prediction[id]?.middles.length ?? 0) !== 0
+        const isPredictionTarget = predictionTargets.includes(id)
+
+        if (isCandidate && id === hoveredCell) {
+          color = active
+        }
+
+        return (
+          <Cell
+            key={id}
+            id={id}
+            color={color}
+            isCandidate={isCandidate}
+            isPredictionTarget={isPredictionTarget}
+            hover={hover}
+            put={put} />
+        )
+      })}
+    </article>
+  )
+}
+
+interface ReversiCellProps {
+  id: number
+  color: Color | null
+  isCandidate: boolean
+  isPredictionTarget: boolean
+  hover: (id: number | null) => void
+  put: (id: number) => void
+}
+
+const Cell: React.FC<ReversiCellProps> = props => {
+  const { id, color, isCandidate, isPredictionTarget, hover, put } = props
+
+  const onClick = React.useCallback(() => {
+    put(id)
+  }, [id, put])
+
+  const onMouseEnter = React.useCallback(() => {
+    hover(id)
+  }, [id, hover])
+
+  return (
+    <div
+      key={id}
+      className="cell"
+      onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      data-is-candidate={isCandidate}
+      data-is-prediction-target={isPredictionTarget}>
+      <div
+        className="stone"
+        data-color={color} />
+    </div>
+  )
+}
