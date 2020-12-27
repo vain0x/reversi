1366727ee368dc02c2ddde8df9c9fb80802d35b9
vain0x <vainzerox@gmail.com>
2020-12-27 17:52:55 +0900

    refactor: Cell を追加

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,15 +1,24 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
+type Color = "WHITE" | "BLACK"
+
 const ReversiContainer: React.FC = () => {
   const whiteCount = 2
   const blackCount = 2
+
+  const active: Color = "BLACK"
+
+
+
+
 
   return <article className="g-reversi g-reversi-container">
     <ReversiBoard />
 
     <div>白石 {whiteCount}</div>
     <div>黒石 {blackCount}</div>
+    <div>手番: {active === "BLACK" ? "黒" : "白"}</div>
   </article>
 }
 
@@ -19,17 +28,33 @@ const ReversiBoard: React.FC = () => {
     cells.push(i)
   }
 
-  return <article className="board">
-    {cells.map(y => (
-      <React.Fragment key={y}>
-        {cells.map(x => (
-          <div key={x} className="cell">
-            <div className="stone" data-color={[null, "black", "white"][(y + x) % 3]} />
-          </div>
-        ))}
-      </React.Fragment>
-    ))}
-  </article>
+  return (
+    <article className="board">
+      {cells.map(y => (
+        <React.Fragment key={y}>
+          {cells.map(x => {
+            const id = y * 3 + x
+            const color = [null, "BLACK", "WHITE"][(y + x) % 3] as Color | null
+            return (<Cell id={id} color={color} />)
+          })}
+        </React.Fragment>
+      ))}
+    </article>
+  )
+}
+
+interface ReversiCellProps {
+  id: number
+  color: Color | null
+}
+
+const Cell: React.FC<ReversiCellProps> = props => {
+  const { id, color } = props
+  return (
+    <div key={id} className="cell">
+      <div className="stone" data-color={color} />
+    </div>
+  )
 }
 
 const Main: React.FC = () => {
